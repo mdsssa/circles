05af9c72ddfa06ea1ee1947f2146febbd7a8098b
disa <mdssa1337@gmail.com>
2026-06-07 22:49:23 +0700

    Initial Commit

---
+++ circles/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  circles
+//
+//  Created by Илья Шевцов on 07.06.2026.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
